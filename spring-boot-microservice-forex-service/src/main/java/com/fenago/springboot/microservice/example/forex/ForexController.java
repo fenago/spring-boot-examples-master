6ef3fadb68592f891ec37b03cd55da04df04a53a
Dr. Ernesto Lee <socrates73@gmail.com>
2023-05-19 07:51:29 -0700

    Update ForexController.java

--- spring-boot-microservice-forex-service/src/main/java/com/fenago/springboot/microservice/example/forex/ForexController.java
+++ spring-boot-microservice-forex-service/src/main/java/com/fenago/springboot/microservice/example/forex/ForexController.java
@@ -1,4 +1,5 @@
-package com.fenago.springboot.microservice.example.forex;
+# package com.fenago.springboot.microservice.example.forex;
+package com.fenago.springboot.microservice.example.forex.springbootmicroserviceforexservice;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
