d0162bcf2f4ce7cb38c3352dd4c511cf00e847f6
Dr. Ernesto Lee <socrates73@gmail.com>
2023-05-19 06:27:15 -0700

    Add files via upload

---
+++ spring-boot-kotlin-basics-configuration/src/main/kotlin/com/fenago/springboot/tutorial/basics/application/configuration/BasicConfiguration.kt
@@ -0,0 +1,12 @@
+package com.fenago.springboot.tutorial.basics.application.configuration
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.stereotype.Component
+
+@Component
+@ConfigurationProperties("basic")
+class BasicConfiguration {
+    var isValue = false
+    var message: String? = null
+    var number = 0
+}
